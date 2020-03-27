4ba3559ef69f7c820f5781ca4787050bf8406343
Terranie Sims <terranie67@gmail.com>
2020-03-27 17:59:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,9 +7,9 @@ def using_unshift (array, string)
 end 
 
 def using_pop (array)
-  array.pop 
+  array.pop
 end
 
 def pop_with_args (array)
-  array.pop("Chihuahua", "Shiba Inu")
+  array.pop(2)
 end
